fbefb7be4cb0b944d847cd4c056d292bb8597f96
oussamaef06 <oussamaelfarihi@gmail.com>
2024-01-29 14:54:18 +0100

    fix : display events work correctly

--- Fullcalendar/app/Http/Controllers/FullCalendarController.php
+++ Fullcalendar/app/Http/Controllers/FullCalendarController.php
@@ -10,14 +10,14 @@ class FullCalendarController extends Controller
 {
     public function index(Request $request){
         if($request->ajax()){
-            $data = Event::whereDate('star', '>=', $request->start)
-                        -> whereDate('end', '<=', $request->end)
-                        ->get(['id', 'event', 'start', 'end']);
+            $data = Event::whereDate('start', '>=', $request->start)
+                        ->whereDate('end', '<=', $request->end)
+                        ->get(['id', 'title', 'start', 'end']);
             return response()->json($data);
         }
         return view('full-calendar');
     }
-
+    
     public function action(Request $request){
         if($request->ajax()){
             if($request->type == 'add'){
