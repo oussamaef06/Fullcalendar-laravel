66dfa28f76acf8477ee01ef3d61651288d19943f
oussamaef06 <oussamaelfarihi@gmail.com>
2024-01-29 14:36:04 +0100

    creating routes for calendar

--- Fullcalendar/routes/web.php
+++ Fullcalendar/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\FullCalendarController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('full-calendar', [FullCalendarController::class, 'index']);
+Route::post('full-calendar/action', [FullCalendarController::class, 'action']);
