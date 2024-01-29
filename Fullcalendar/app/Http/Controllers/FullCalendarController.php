9dc052b580e93b4094bf94c9a4319f7835862367
oussamaef06 <oussamaelfarihi@gmail.com>
2024-01-29 14:38:03 +0100

    creating caledar controller

---
+++ Fullcalendar/app/Http/Controllers/FullCalendarController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Event;
+
+class FullCalendarController extends Controller
+{
+    public function index(Request $request){
+        if($request->ajax()){
+            $data = Event::whereDate('star', '>=', $request->start)
+                        -> whereDate('end', '<=', $request->end)
+                        ->get(['id', 'event', 'start', 'end']);
+            return response()->json($data);
+        }
+        return view('full-calendar');
+    }
+
+    public function action(Request $request){
+        if($request->ajax()){
+            if($request->type == 'add'){
+                $event = Event::create([
+                    'title' => $request->title,
+                    'start' => $request->start,
+                    'end' => $request->end
+                ]);
+                return response()->json($event);
+            }
+        }
+    }
+}
